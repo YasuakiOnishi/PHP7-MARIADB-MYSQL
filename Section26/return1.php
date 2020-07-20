a51e229f8cea6c86177e83899e2931f22fee535d
Yasuaki Onishi <yasucrohn11@gmail.com>
2020-07-20 15:10:23 +0900

    Section26返り値

---
+++ Section26/return1.php
@@ -0,0 +1,21 @@
+<?php
+$str = "abcdefghijklemnop";
+$byte = 16;
+
+$flag = checkByte($str, $byte);
+if($flag){
+    print "OKです。";
+}else{
+    print "エラーです。";
+    print $byte;
+    print "バイトを超えています";
+}
+
+function checkByte($str, $byte){
+    $strlen = strlen($str);
+    if($strlen <= $byte){
+        return true;
+    }
+    return false;
+}
+?>
