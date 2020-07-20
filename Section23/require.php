cc50f8f8d720cabaabc1741b304c66509a2ad285
Yasuaki Onishi <yasucrohn11@gmail.com>
2020-07-20 15:08:41 +0900

    Section23 require文・include文

---
+++ Section23/require.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<title>PHPのテスト</title>
+</head>
+<body>
+
+<?php
+require("data.php");
+print "$name さま<br>";
+print "$message<br>";
+?>
+
+</body>
+</html>
